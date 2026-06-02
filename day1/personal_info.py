877930af64aed8bb7d72c44a8a6f612537bac4ee
Purujith27 <purujith27satya@gmail.com>
2026-06-02 09:40:37 +0530

    Create personal_info.py

---
+++ day1/personal_info.py
@@ -0,0 +1,7 @@
+name = "Purujith.S"
+age = 20
+college = "VEL TECH UNIVERSITY"
+branch = "CSD"
+cgpa = 7.5
+
+print(f"My name is {name}. I am {age} studying {branch} at {college} with {cgpa} CGPA.")
